2d76f29765675224bf02f8254edd8650abccdc1b
HAYASHI Kentaro <kenhys@gmail.com>
2016-05-03 19:25:54 +0900

    Indent

--- provision.sh
+++ provision.sh
@@ -9,9 +9,9 @@ DEBIAN_RELEASE=$(lsb_release -c | awk '{print $2}')
 
 cd ~/debexpo/
 if [ $DEBIAN_RELEASE = "wheezy" ]; then
-if ! grep -q backports /etc/apt/sources.list; then
-    echo 'deb http://http.debian.net/debian/ wheezy-backports main contrib non-free' | sudo sh -c 'cat >> /etc/apt/sources.list'
-fi
+    if ! grep -q backports /etc/apt/sources.list; then
+        echo 'deb http://http.debian.net/debian/ wheezy-backports main contrib non-free' | sudo sh -c 'cat >> /etc/apt/sources.list'
+    fi
 fi
 if ! grep -q deb-src /etc/apt/sources.list; then
     echo "deb-src http://mirrors.kernel.org/debian ${DEBIAN_RELEASE} main" | sudo sh -c 'cat >> /etc/apt/sources.list'
@@ -24,12 +24,12 @@ sudo debconf-set-selections <<< "postfix postfix/mailname string debexpo-dev"
 sudo apt-get update
 case $DEBIAN_RELEASE in
     wheezy)
-sudo apt-get install postfix python-lxml libapt-pkg-dev python-pip python-dev python-virtualenv python-django=1.7.1-1~bpo70+1 --yes
-sudo apt-get install --yes python-fedmsg -t wheezy-backports
-;;
+        sudo apt-get install postfix python-lxml libapt-pkg-dev python-pip python-dev python-virtualenv python-django=1.7.1-1~bpo70+1 --yes
+        sudo apt-get install --yes python-fedmsg -t wheezy-backports
+        ;;
     jessie)
-sudo apt-get install postfix python-lxml libapt-pkg-dev python-pip python-dev python-virtualenv python-django python-fedmsg python-apt --yes
-;;
+        sudo apt-get install postfix python-lxml libapt-pkg-dev python-pip python-dev python-virtualenv python-django python-fedmsg python-apt --yes
+        ;;
 esac
 sudo apt-get build-dep --yes python-lxml
 echo '* discard:' | sudo sh -c 'cat > /etc/postfix/discard-transport'
@@ -43,7 +43,7 @@ if ! [ -f ~/debexpo/venv/bin/python ]; then
 fi
 . venv/bin/activate
 if [ $DEBIAN_RELEASE = "wheezy" ]; then
-pip install https://launchpad.net/python-apt/main/0.7.8/+download/python-apt-0.8.5.tar.gz
+    pip install https://launchpad.net/python-apt/main/0.7.8/+download/python-apt-0.8.5.tar.gz
 fi
 pip install --editable .
 paster setup-app development.ini
